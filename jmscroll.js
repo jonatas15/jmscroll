35c7a3157481a63e5040cd76ab6b5e5e116cd481
blixabargeld <nikolaos.i.papadopoulos@gmail.com>
2014-12-24 15:49:26 +0200

    More Options For The Progress Animation

--- jmscroll.js
+++ jmscroll.js
@@ -15,8 +15,29 @@
     footerPadding:0,
     footerVisible:true,
     loadingContentDelay:1000,
+    callback:false,
+    progressType:'bar', /*bar | html | spinner*/
+    /*Parameters For html Animation*/
+    loadingHtml:'Loading More',
+    /*Parameters For js Animation*/
     progressBarSpeed:10,
-    callback:false
+    /*Parameters For spinner Animation*/
+    spinnerLines:13, //Lines To Draw
+    spinnerLength:10, //Length Of Each Line
+    spinnerWidth:10, //Line Thickness
+    spinnerRadius:20, //Radius Of The Inner Circle
+    spinnerCorners:1, //Corner Roundness (0..1)
+    spinnerRotate:0, //The Rotation Offset
+    spinnerDirection:1, //1: clockwise, -1: counterclockwise
+    spinnerColor:'#000', // #rgb or #rrggbb
+    spinnerSpeed:1, //Rounds Per Second
+    spinnerTrail:60, // Afterglow Percentage
+    spinnerShadow:false, //Whether To Render A Shadow Or Not
+    spinnerHwaccel:false, //Whether To Use Hardware Acceleration Or Not
+    spinnerClassName:'spinner', //CSS Class
+    spinnerZIndex:2e9, //Defaults To 2000000000
+    spinnerTop:'50%', //Top Position Relative To Parent
+    spinnerLeft:'50%' //Left Position Relative To Parent
   }
  };
 
@@ -33,7 +54,18 @@
   $event.data('jmscroll', $.extend({}, _data, {initialized: true, waiting: false, nextHref: _nextHref}));
   if(_options.autoTrigger === true && _options.footerVisible === false) $(_options.footerSelector).hide();
   _wrapInnerContent();
+  if(_options.progressType == 'html') _preloadImage();
+  if(_options.progressType == 'spinner') { var spinner; _spinnerOptions(); }
   _setBindings();
+  
+  //If progressType = 'html' Check If An Image Is Given and Preload It
+  function _preloadImage() {
+    var src = $(_options.loadingHtml).filter('img').attr('src');
+    if(src) {
+      var image = new Image();
+      image.src = src;
+    }
+  }
 
   function _wrapInnerContent() {
     if(!$event.find('.scroll-wrapper').length) { //Wrap If It Isn't Already Wrapped
@@ -99,16 +131,23 @@
     var $inner = $event.find('div.scroll-wrapper').first(),
     data = $event.data('jmscroll');
     data.waiting = true;
-    $inner.append('<div class="scroll-added" />').children('.scroll-added').last().html('<div class="progress-bar"><span></span></div>');
-
-    _progressBar();    
+    
+    //Progress Animation or Message
+    var loaderHtml = '';
+    if(_options.progressType == 'html') loaderHtml = '<div class="progress-div progress-html">' + _options.loadingHtml + '</div>';
+      else if(_options.progressType == 'spinner') loaderHtml = '<div class="progress-div progress-spinner"></div>';
+	else loaderHtml = '<div class="progress-div progress-bar"><span></span></div>';
+    $inner.append('<div class="scroll-added" />').children('.scroll-added').last().html(loaderHtml);
+    if(_options.progressType == 'spinner') _spinner();
+    if(_options.progressType == 'bar') _progressBar();    
 
     return $event.animate({scrollTop: $inner.outerHeight()}, _options.loadingContentDelay, function() {
       $inner.find('div.scroll-added').last().load(data.nextHref, function(response, status, xhr) {
+      if(_options.progressType == 'spinner') spinner.stop(); //Regardless Of Response Status
       if(status === 'error') {
 	_debug('error', 'Error Status Returned From load(). Calling destroy()');
 	if(_options.footerVisible === false) $(_options.footerSelector).show();
-	$('.progress-bar').hide();
+	$('.progress-div').hide();
 	return _destroy();
       }
       var $next = $(this).find(_options.nextSelector).first();
@@ -120,18 +159,6 @@
       });
     });
   }
-
-  function _progressBar() {
-    var counter = 0;
-    var interval = setInterval(function() {
-      counter++;
-      var progressBarWidth = counter%100 * $('.progress-bar').width() / 100;
-      $('.progress-bar').find('span').animate({ width: progressBarWidth }, _options.progressBarSpeed);
-      if(counter * _options.progressBarSpeed > _options.loadingContentDelay) { //Clear Interval After loadingContentDelay Milliseconds
-	clearInterval(interval);
-      }
-    }, _options.progressBarSpeed); 
-  }  
 
   function _checkNextHref(data) {
     data = data || $event.data('jmscroll');
@@ -151,6 +178,46 @@
     return _$document.removeData('jmscroll')
       .find('.scroll-wrapper').children().unwrap()
       .filter('.scroll-added').children().unwrap();
+  }
+  
+  //If progressType = 'spinner' Create Spinner According To Parameters Given
+  function _spinnerOptions() {
+    var spinnerOptions = {
+      lines:_options.spinnerLines,
+      length:_options.spinnerLength,
+      width:_options.spinnerWidth,
+      radius:_options.spinnerRadius,
+      corners:_options.spinnerCorners,
+      rotate:_options.spinnerRotate,
+      direction:_options.spinnerDirection,
+      color:_options.spinnerColor,
+      speed:_options.spinnerSpeed,
+      trail:_options.spinnerTrail,
+      shadow:_options.spinnerShadow,
+      hwaccel:_options.spinnerHwaccel,
+      className:_options.spinnerClassName,
+      zIndex:_options.spinnerZIndex,
+      top:_options.spinnerTop,
+      left:_options.spinnerLeft
+    };
+    spinner = new Spinner(spinnerOptions); 
+  }  
+  
+  function _spinner() {
+    spinner.spin();
+    $('.progress-div').append(spinner.el);
+  }
+
+  function _progressBar() { /*progressType=='bar'*/
+    var counter = 0;
+    var interval = setInterval(function() {
+      counter++;
+      var progressBarWidth = counter%100 * $('.progress-div').width() / 100;
+      $('.progress-div').find('span').animate({ width: progressBarWidth }, _options.progressBarSpeed);
+      if(counter * _options.progressBarSpeed > _options.loadingContentDelay) { //Clear Interval After loadingContentDelay Milliseconds
+	clearInterval(interval);
+      }
+    }, _options.progressBarSpeed); 
   }
 
   //Safe Console Debug
